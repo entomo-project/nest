67da479b8ec343456c0f9e570981d6c37ff3dddd
Benjamin Michalski <benjamin.michalski@gmail.com>
2016-07-24 23:54:55 +0200

    Work in progress.

---
+++ deploy.js
@@ -0,0 +1,30 @@
+const assert = require('assert')
+const spawn = require('child_process').spawn
+const config = require('./deploy.config.json')
+
+const rsync = spawn(
+  'rsync',
+  [
+    '-e',
+    'ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null',
+    '-avz',
+    '--exclude',
+    '.idea',
+    '--exclude',
+    '.git',
+    '.',
+    config.username + '@' + config.hostname + ':/home/r/nest'
+  ]
+)
+
+rsync.stdout.on('data', (data) => {
+  console.log(""+data);
+});
+
+rsync.stderr.on('data', (data) => {
+  console.log(""+data);
+});
+
+rsync.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
